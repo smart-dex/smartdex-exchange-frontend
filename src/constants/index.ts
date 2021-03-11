0347eeecc7b84f66731837ec26e134b3727804ad
vietnt <vietnt>
2021-03-11 10:43:03 +0700

    add config

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
 
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0x9D94554F66421b734656e3D53Ed7a84212d09722'
 
 // a list of tokens by chain
 type ChainTokenList = {
